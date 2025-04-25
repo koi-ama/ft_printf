0a9ee0acbf718406dc564c3bffaa83aa0783e2f9
Koichiro Amakasu <kamakasu@c5r2s5.42tokyo.jp>
2025-04-25 16:48:50 +0900

    Fix va_list handling for Linux compatibility

--- src/ft_vdprintf.c
+++ src/ft_vdprintf.c
@@ -16,16 +16,21 @@ int	ft_vdprintf(int fd, const char *format, va_list ap)
 {
 	int	ret;
 	int	w_ret;
+	va_list ap_copy;
 
+	va_copy(ap_copy, ap);
 	ret = 0;
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			w_ret = ft_printf_switch(format, &ap, fd);
+			w_ret = ft_printf_switch(format, ap_copy, fd);
 			if (w_ret < 0)
+			{
+				va_end(ap_copy);
 				return (-1);
+			}
 			ret += w_ret;
 		}
 		else
@@ -37,5 +42,6 @@ int	ft_vdprintf(int fd, const char *format, va_list ap)
 		}
 		format++;
 	}
+	va_end(ap_copy);
 	return (ret);
 }
